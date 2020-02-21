a47a725eb283a4a5a320fd9479e6b65da0a061d9
Grunaf <60441662+Grunaf@users.noreply.github.com>
2020-02-22 01:29:51 +0300

    Make some changes

--- app/src/main/java/com/example/learnlanguage/LlanguageDatabaseHelper.java
+++ app/src/main/java/com/example/learnlanguage/LlanguageDatabaseHelper.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 
 class LlanguageDatabaseHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "learlanguage";
-    private static final int DB_VERSION = 18;
+    private static final int DB_VERSION = 19;
     private String sql = "CREATE TABLE WORDS (_id INTEGER PRIMARY KEY AUTOINCREMENT, WORD TEXT, TRANSLATE TEXT, IMAGE_SRC TEXT);";
 
     LlanguageDatabaseHelper(Context context) {
@@ -21,11 +21,45 @@ class LlanguageDatabaseHelper extends SQLiteOpenHelper {
         insertWord(db,"Муьгьуьбат", "Любовь", "love");
         insertWord(db,"килфет", "Семья", "family");
         insertWord(db,"гада", "Мальчик", "boy");
+        insertWord(db,"йагъйагъун", "Битва", "battle");
+        insertWord(db,"къиб", "Лягушка", "frog");
+        insertWord(db,"сев", "Медведь", "bear");
+        insertWord(db,"чепелукь", "Бабочка", "butterfly");
+        insertWord(db,"верч", "Курица", "chicken");
+        insertWord(db,"чин", "Лицо", "face");
+        insertWord(db,"сик1", "Лиса", "fox");
+        insertWord(db,"аслан", "Лев", "lion");
+        insertWord(db,"вирт", "Мёд", "honey");
+        insertWord(db,"мурк", "Лёд", "ice");
+        insertWord(db,"варз", "Луна", "moon");
+        insertWord(db,"кьиф", "Мышь", "mouse");
+        insertWord(db,"капан юкь", "Ладонь", "palms");
+        insertWord(db,"инсан", "Люди", "people");
+        insertWord(db,"къав", "Крыша", "roofs");
+        insertWord(db,"т1ур", "Ложка", "spoon");
+        insertWord(db,"гад", "Лето", "summer");
+        insertWord(db,"къугь", "Лебедь", "swan");
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        insertWord(db,"йагъйагъун", "Битва", "battle");
         insertWord(db,"къиб", "Лягушка", "frog");
-
+        insertWord(db,"сев", "Медведь", "bear");
+        insertWord(db,"чепелукь", "Бабочка", "butterfly");
+        insertWord(db,"верч", "Курица", "chicken");
+        insertWord(db,"чин", "Лицо", "face");
+        insertWord(db,"сик1", "Лиса", "fox");
+        insertWord(db,"аслан", "Лев", "lion");
+        insertWord(db,"вирт", "Мёд", "honey");
+        insertWord(db,"мурк", "Лёд", "ice");
+        insertWord(db,"варз", "Луна", "moon");
+        insertWord(db,"кьиф", "Мышь", "mouse");
+        insertWord(db,"капан юкь", "Ладонь", "palms");
+        insertWord(db,"инсан", "Люди", "people");
+        insertWord(db,"къав", "Крыша", "roofs");
+        insertWord(db,"т1ур", "Ложка", "spoon");
+        insertWord(db,"гад", "Лето", "summer");
+        insertWord(db,"къугь", "Лебедь", "swan");
     }
 
     public static void insertWord(SQLiteDatabase db, String word, String translate, String imageName ) {
